0b2049ce4f71d2ae1476520674e4c52547b6a040
vchandwani <varunchandwani@gmail.com>
2020-02-04 15:09:37 +1100

    Chnages for ingredients remove and add
    Chnages for ingredients remove and add

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,13 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''});
+  //  Data and function to update the data
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
+    props.onAddIngredient({title: enteredTitle, amount : enteredAmount})
     // ...
   };
 
@@ -17,11 +20,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title:event.target.value})} />
+            <input type="text" id="title" value={enteredTitle} 
+              onChange={
+                event => { 
+                setEnteredTitle(event.target.value)
+              }} 
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount:event.target.value})} />
+            <input type="number" id="amount" value={enteredAmount} 
+              onChange={
+                event => { 
+                setEnteredAmount(event.target.value)
+              }} 
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients , setUserIngredients ] = useState([]);
+
+  const addIngredientsHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString() , ...ingredient}]);
+  }
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+  }
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientsHandler}/>
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
