c73e0626a239b70c1c36ebfd844387827811160c
vchandwani <varunchandwani@gmail.com>
2020-02-05 11:32:48 +1100

    Use ref added and timer added to stop multiple request. Error Modal showed
    Use ref added and timer added to stop multiple request. Error Modal showed

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = () => {
   // USed at root level
   const [ userIngredients , setUserIngredients ] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   //Side effect, logic that effects your application
   //Acts like componentDidupdate
-  useEffect(() => {
-    fetch('https://react-hook-8e8ad.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients =[];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    });
-  },[]);
   // With empty array acts Like componentDidMount and run only once
+  useEffect(() => {
+    console.log('rendering', userIngredients);
+  },[userIngredients]);
 
+  //Usecall back stops re-render
+  const filteredIngredientsHandler = useCallback(fileteredIngredients => {
+    setUserIngredients(fileteredIngredients);
+  },[])
   const addIngredientsHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://react-hook-8e8ad.firebaseio.com/ingredients.json', {
       method  : 'POST',
       body : JSON.stringify(ingredient),
       headers: {'Content-Type': 'application/json'}
     })
     .then(response => {
+      setIsLoading(false);
       return response.json();
     }).then(responseBody => {
       setUserIngredients(prevIngredients => [...prevIngredients, {id: responseBody.name , ...ingredient}]);
@@ -41,14 +38,32 @@ const Ingredients = () => {
     .catch(err => err);
   }
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+    setIsLoading(true);
+    fetch(`https://react-hook-8e8ad.firebaseio.com/ingredients/${ingredientId}.json`, {
+      method  : 'DELETE'
+    })
+    .then(response => {
+      setIsLoading(false);
+      setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+    })
+    .catch(error => {
+      //Executed in Sync, batched together
+      setError(error.message);
+      setIsLoading(false);
+    })
+  }
+  const clearError = () => {
+    //Executed in Sync, batched together
+    setError(null);
+    setIsLoading(false);
   }
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientsHandler}/>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientsHandler} loading={isLoading}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         {/* Need to add list here! */}
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect, useRef, useReducer} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [eneteredFilter, setEnteredFiler] = useState('');
+  const inputRef = useRef();
+  useEffect(()=> {
+    const timer = setTimeout(()=>{
+      if(eneteredFilter === inputRef.current.value){
+        const query = eneteredFilter.length ===0 ?'': `?orderBy="title"&equalTo="${eneteredFilter}"`;
+        fetch('https://react-hook-8e8ad.firebaseio.com/ingredients.json'+query)
+        .then(response => response.json())
+        .then(responseData => {
+          const loadedIngredients =[];
+          for(const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+          onLoadIngredients(loadedIngredients);
+        });
+      }
+    },500);
+    //cleanup function, behave like component unmount
+    return () => {}
+    clearTimeout(timer);
+  }, [eneteredFilter,onLoadIngredients,inputRef]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text"
+          ref={inputRef}
+            value={eneteredFilter}
+            onChange={event => setEnteredFiler(event.target.value)} />
         </div>
       </Card>
     </section>
